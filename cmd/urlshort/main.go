d01987fa73a90f221ce6f3553343098603fd7657
Omar Chammaa <chammaa.omar@gmail.com>
2020-05-30 23:39:28 -0400

    user can use json or yaml file; implemented relevant switch

--- cmd/urlshort/main.go
+++ cmd/urlshort/main.go
@@ -12,11 +12,11 @@ import (
 )
 
 var yamlPtr = flag.String("yaml", "", "path to yaml file mapping short paths to full urls")
+var jsonPtr = flag.String("json", "", "path to json file mapping short paths to full urls. Ignored if yaml flag is used.")
 
 func main() {
 	flag.Parse()
 	mux := defaultMux()
-	var routing []byte
 
 	// Build the MapHandler using the mux as the fallback
 	pathsToUrls := map[string]string{
@@ -28,14 +28,12 @@ func main() {
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
 	if len(*yamlPtr) > 0 {
-		routing = readFile(*yamlPtr)
+		startHandler(*yamlPtr, "yaml", mapHandler)
+	} else if len(*jsonPtr) > 0 {
+		startHandler(*jsonPtr, "json", mapHandler)
+	} else {
+		log.Fatal("One of yaml or json flags is required")
 	}
-	yamlHandler, err := urlshort.YAMLHandler(routing, mapHandler)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Starting the server on localhost:8080")
-	http.ListenAndServe("localhost:8080", yamlHandler)
 }
 
 func defaultMux() *http.ServeMux {
@@ -56,4 +54,23 @@ func readFile(path string) []byte {
 		log.Fatal(err)
 	}
 	return routings
+}
+
+// startHandler invokes the relevant handler based on the routing file
+// the user chose, i.e. json or yaml.
+func startHandler(path string, dataType string, fallback http.HandlerFunc) {
+	routing := readFile(path)
+	var handler http.HandlerFunc
+	var err error
+	switch dataType {
+	case "yaml":
+		handler, err = urlshort.YAMLHandler(routing, fallback)
+	case "json":
+		handler, err = urlshort.JSONHandler(routing, fallback)
+	}
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on localhost:8080")
+	http.ListenAndServe("localhost:8080", handler)
 }
